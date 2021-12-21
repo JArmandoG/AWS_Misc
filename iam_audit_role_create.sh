da9f42aed91a16ec2ea3ce29ecb603f49c05efad
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2021-12-21 10:54:49 -0600

    Fix Typos
    ARN referencing a wrong policy

--- iam_audit_role_create.sh
+++ iam_audit_role_create.sh
@@ -6,8 +6,8 @@ aws iam create-group --group-name Auditor-group
 aws iam create-policy --policy-name Auditor-Policy --policy-document file://Audit-Policy.json
 aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::aws:policy/SecurityAudit
 aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::aws:policy/job-function/ViewOnlyAccess
-aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::${ACCOUNT_ID}:Auditor-group
+aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/Auditor-Policy
 aws iam create-user --user-name auditor
 aws iam add-user-to-group --user-name auditor --group-name Auditor-group
-aws iam create-access-key --user-name Auditor-group
+aws iam create-access-key --user-name auditor
 unset ACCOUNT_ID AWS_DEFAULT_PROFILE
