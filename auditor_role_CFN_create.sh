26b8b5445352fb250dd4be20ef29f1015f0c9939
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2021-12-20 21:15:27 -0600

    Add files via upload

---
+++ auditor_role_CFN_create.sh
@@ -0,0 +1,11 @@
+# USAGE:
+# ./this_script.sh <ACCOUNT_ID> <CFN_CREATION_FILE.yaml>
+
+ACCOUNT_ID=$1
+CFN_FILENAME=$2
+
+aws cloudformation create-stack \
+  --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM \
+  --template-body "file://$CFN_FILENAME" \
+  --stack-name "AuditorExecRole" \
+  --parameters "ParameterKey=AuthorisedARN,ParameterValue=arn:aws:iam::$ACCOUNT_ID:root"
