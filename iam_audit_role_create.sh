d16c7e931e9d595126bbc36a36ead0621af4162f
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2021-12-20 21:46:31 -0600

    Update iam_audit_role_create.sh

--- iam_audit_role_create.sh
+++ iam_audit_role_create.sh
@@ -3,7 +3,7 @@ export AWS_DEFAULT_PROFILE=default
 export ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' | tr -d '"')
 aws iam create-group --group-name Auditor
 # Keep the Auditor-policy.json in the same folder
-aws iam create-policy --policy-name Auditor-Policy --policy-document file://Auditor-Policy.json
+aws iam create-policy --policy-name Auditor-Policy --policy-document file://Audit-Policy.json
 aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::aws:policy/SecurityAudit
 aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::aws:policy/job-function/ViewOnlyAccess
 aws iam attach-group-policy --group-name Auditor-group --policy-arn arn:aws:iam::${ACCOUNT_ID}:Auditor-group
